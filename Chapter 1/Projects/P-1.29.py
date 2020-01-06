1258ddd7978219d54c1af44dd57960df3c6823d9
Julián Jorge Sanmartino <58923155+jjsanmartino03@users.noreply.github.com>
2020-01-06 18:50:00 -0300

    Added P-1.29.py

--- Chapter 1/Projects/P-1.29.py
+++ Chapter 1/Projects/P-1.29.py
@@ -3,3 +3,20 @@ P-1.29
 Write a Python program that outputs all possible strings formed by using
 the characters c , a , t , d , o , and g exactly once.
 """
+import copy
+
+
+def my_function(alr, left):
+    print(alr)
+    if left:
+        for i in left:
+            newl = copy.deepcopy(left)
+            newl.remove(i)
+            my_function(alr + list(i), newl)
+
+
+original = ["c", "a", "t", "d", "g", "o"]
+for i in original:
+    new_list = copy.deepcopy(original)
+    new_list.remove(i)
+    my_function(list(i), new_list)
