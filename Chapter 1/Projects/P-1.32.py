66d5f12cfccd789cdfe720cd684eeff034363a08
Julián Jorge Sanmartino <58923155+jjsanmartino03@users.noreply.github.com>
2020-01-06 22:15:15 -0300

    Added P-1.32 and a needed module

---
+++ Chapter 1/Projects/P-1.32.py
@@ -0,0 +1,26 @@
+"""
+P-1.32 
+Write a Python program that can simulate a simple calculator, using the
+console as the exclusive input and output device. That is, each input to the
+calculator, be it a number, like 12.34 or 1034, or an operator, like + or =,
+can be done on a separate line. After each such input, you should output
+to the Python console what would be displayed on your calculator.
+"""
+import calc  # This is an inline calculator I had already programmed
+
+complete = ""
+ans = 0
+print("To exit the calculator write 'quit()'")
+print("The unique word available is 'ans'")
+while True:
+    a = input()
+    if a == "quit()":
+        break
+    if a == "=":
+        ans = calc.start(complete, ans)
+        print(ans)
+        complete = ""
+    else:
+        complete += a
+        print(complete)
+
