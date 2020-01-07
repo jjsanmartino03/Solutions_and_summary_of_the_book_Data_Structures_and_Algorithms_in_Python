a96d45473a5b287000e2c8cf449c3cd754608dc0
Julián Jorge Sanmartino <58923155+jjsanmartino03@users.noreply.github.com>
2020-01-06 22:14:15 -0300

    Changed C-1.13.py

--- Chapter 1/Creativity/C-1.13.py
+++ Chapter 1/Creativity/C-1.13.py
@@ -5,7 +5,7 @@ integers, so that the numbers are listed in the opposite order than they
 were before, and compare this method to an equivalent Python function
 for doing the same thing.
 """
-
+import calc
 #1.return a list of the given list but inversely looped
 
 data = [2, 73, 5, 89]
