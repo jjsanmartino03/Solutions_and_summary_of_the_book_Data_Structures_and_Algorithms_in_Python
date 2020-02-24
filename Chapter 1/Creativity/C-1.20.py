bc2edf553b50f5128358b3f33316f6baec71c884
Julián Jorge Sanmartino <58923155+jjsanmartino03@users.noreply.github.com>
2020-02-24 00:31:28 -0300

    Update C-1.20.py

--- Chapter 1/Creativity/C-1.20.py
+++ Chapter 1/Creativity/C-1.20.py
@@ -1,8 +1,10 @@
 """
-C-1.19 
-Demonstrate how to use Python’s list comprehension syntax to produce
-the list [ a , b , c , ..., z ], but without having to type all 26 such
-characters literally.
+C-1.20 
+Python’s random module includes a function shuffle(data) that accepts a list of elements
+and randomly reorders the elements so that each possible order occurs with equal probability.
+The random module includes a more basic function randint(a, b) that returns a uniformly
+random integer from a to b (including both endpoints). Using only the randint function,
+implement your own version of the shuffle function.
 """
 
 from random import randint
@@ -17,10 +19,6 @@ def shuffle(data):
             new_index = randint(0, (len(data) - 1))
         used.append(new_index)
         positions.setdefault(i, new_index)
-    for i in positions:
-        data[positions[i]] = i
+    for key, value in positions.items():
+        data[value] = key
 
-
-app = [1, 3, 7, 8]
-shuffle(app)
-print(app)
