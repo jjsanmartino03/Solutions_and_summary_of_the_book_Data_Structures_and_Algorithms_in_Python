3285d7163a30628ae3b5df6967f596e1010cfa54
Julián Jorge Sanmartino <58923155+jjsanmartino03@users.noreply.github.com>
2020-04-26 19:41:31 -0300

    Finished Chapter1 notebook

--- Chapter 1/Projects/P-1.33.py
+++ Chapter 1/Projects/P-1.33.py
@@ -7,38 +7,62 @@ operation is performed. Minimally, your calculator should be able to process
 the basic arithmetic operations and a reset/clear operation.
 """
 
-import calc
 
-num = ""
-opers = ["+", "-", "*", "/", "**"]
-complete = ""
-ans = 0
-print(
-    "The only supported inputs that are not operators or numbers are: 'off', 'cls', 'ans' and '√'"
-)
-print(0)
-while True:
-    pressed = input("> ")
-    if pressed == "off":
-        break
-    if pressed == "cls":
-        num = ""
-        complete = 0
-        ans = 0
-        print(0)
-        continue
-    if pressed in opers:
-        print(num)
-        complete += num
-        complete += pressed
-        num = ""
-    elif pressed == "=":
-        complete += num
-        ans = calc.start(complete, ans)
-        print(ans)
-        complete = ""
-        num = ""
-    else:
-        num += pressed
-        print(num)
+def start_handheld_calculator():
+    opers = ["+", "-", "*", "/", "**"]
+    cur_oper = ""
+    current = ""
+    inp = input()
 
+    while inp != "esc":
+
+        if inp not in opers:
+
+            if inp.isdigit() and cur_oper:
+                current += inp
+                print(current)
+
+            elif inp == "." and "." not in current and cur_oper:
+                current += inp
+                print(current)
+
+            elif (inp.isdigit() or inp == ".") and not cur_oper:
+                current = inp
+
+            elif inp == "cls":
+                current = ""
+                result = 0
+                cur_oper = ""
+                print(0)
+
+            elif inp == "=":
+                result = eval(f"{result}{cur_oper}{current}")
+                cur_oper = ""
+                print(result)
+                current = result
+
+            else:
+                print("Syntax Error, values resetted to 0")
+                result = 0
+                current = ""
+        else:
+            if cur_oper and inp in "+-" and not current:
+                current += inp
+                print(current)
+
+            elif not cur_oper:
+                cur_oper = inp
+                result, current = current, ""
+
+            else:
+                bef_oper, cur_oper = cur_oper, inp
+
+                result = eval(f"{result}{bef_oper}{current}")
+                bef_oper = ""
+                print(result)
+
+                current = ""
+        inp = input()
+
+
+start_handheld_calculator()
